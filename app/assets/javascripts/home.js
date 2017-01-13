e6e9cdc89c7c8d42b636b69677ab171de1248193
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-01-13 23:39:45 +0100

    Add displaying tasks and styles

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,27 @@
 $(document).on("turbolinks:load", function(){
 	$.getJSON("/tasks",function(data){
-		var tasks = [];
+		var tasks = "";
+		var task_done = [];
+
 		$.each(data, function(key,value){
+			tasks += "<div class='col-lg-3 col-lg-offset-1 well'>";
+
 			$.each(value, function(key, value){
-				tasks.push(key + ' ' + value);
+				if(key != "done")
+					tasks += "<strong>" + value + "</strong>";
+				else
+					task_done.push(value)
 			});
+
+			tasks += "</div>";
 		});
 
-		$(".tasks").text(tasks);
+		$("#tasks").append(tasks);
+		$('.well').each(function(i, obj){
+			if(task_done[i])
+				$(obj).addClass('task-done');
+			else
+				$(obj).addClass('task-to-do');
+		});
 	});
 });
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
-    render json: @tasks.to_json 
+    render json: @tasks.to_json(:except => [:id ,:created_at, :updated_at]) 
   end
 
   def create
