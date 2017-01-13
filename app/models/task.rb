b4b8ba0e923d326d4e3103c1d648081d1e02a87e
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-01-13 14:11:22 +0100

    Add Task validation and specs

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: {minimum: 5, maximum: 20}, uniqueness: true
+  validates :done, inclusion: { in: [true, false] }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+  context 'Validation' do
+
+    subject { described_class.new(name: 'test task', done: nil) }
+
+    it 'is valid with valid attributes' do
+      subject.name = 'My task'
+      subject.done = false;
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid when name too short' do
+      subject.name = 'Tes'
+      expect(subject).to_not be_valid 
+    end
+
+    it 'is not valid when name too long' do
+      subject.name = 'develop photos and go to the cinema'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid when same name' do
+      subject.name = 'my task'
+      subject.save
+      same_name = Task.new(name: 'my task')
+      expect(same_name).to_not be_valid
+    end
+
+    it 'is not valid when done nil' do
+      expect(subject).to_not be_valid
+    end
+  end
+end 
