c4b874d22016659d1e00ed61913a99845119ea15
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-01-13 14:58:06 +0100

    Change route for get_done action in Task controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   resources :tasks, except: [:show, :destroy, :edit, :update] do
-    get 'get_done', on: :member 
+    get :get_done, on: :member 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
