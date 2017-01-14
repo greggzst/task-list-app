2d9825269d5da19042e6266f3eec1da7d0920eb4
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-01-14 10:47:37 +0100

    Add new route to Task resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   
-  resources :tasks, only: [:index, :create] do
+  resources :tasks, only: [:index, :new, :create] do
     get :get_done, on: :member 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
