d5a484b5f39d59a5df5a4e96b43b283fff83d6af
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-01-17 20:15:21 +0100

    Add destroy route to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   
-  resources :tasks, only: [:index, :new, :create] do
+  resources :tasks, only: [:index, :new, :create, :destroy] do
     get :get_done, on: :member 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
